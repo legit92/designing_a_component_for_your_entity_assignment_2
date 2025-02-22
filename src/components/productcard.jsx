b4cb5b0ffe53b1420f3de9cefdc3802d5647ecb3
legit92 <nitindhake3@gmail.com>
2025-02-22 10:32:08 +0530

    completed

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
-// write product card here
+import React from "react";
+import ViewProductButton from "./button";
+
+const productCard = () => {
+    const product = {
+        image: "https://placehold.co/150",
+        name: "product",
+        price: "$230",
+    }
+    return (
+        <div>
+            <img src={product.image} alt="product Image" />
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <ViewProductButton />
+        </div>
+    );
+};
+
+export default productCard;
